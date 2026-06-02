48db6b1887389c04755310075e3a1ca4f3fb9aca
Onur AYDINOĞLU <onur.aydinoglu.98@gmail.com>
2026-06-02 20:40:22 +0300

    hatalar giderildi

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -49,7 +49,7 @@ export default function HomeHeader({
         </Text>
 
         <Text style={styles.welcomeText} numberOfLines={1} ellipsizeMode="tail">
-          Hoş Geldin{name ? `, ${name}` : ""}!
+          {name ? `, ${name}` : ""}
         </Text>
       </View>
 

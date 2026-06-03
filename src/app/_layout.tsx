40233efd38f6e56882153522e070440179dc035d
Onur AYDINOĞLU <onur.aydinoglu.98@gmail.com>
2026-06-03 20:09:00 +0300

    tasarım iyileştirmeleri

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,29 @@
+import {
+  Manrope_400Regular,
+  Manrope_500Medium,
+  Manrope_600SemiBold,
+  Manrope_700Bold,
+  Manrope_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/manrope";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+
 import "../../global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Manrope_400Regular,
+    Manrope_500Medium,
+    Manrope_600SemiBold,
+    Manrope_700Bold,
+    Manrope_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="auto" />
